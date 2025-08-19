420b902ddd811512ff3f0347f4ad7819f9531806
AquilesFreitas <aquilesfreitas350@gmail.com>
2025-08-19 20:56:59 -0300

    teste log-in

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
-// firebase.js
+// Importando via CDN (funciona direto em HTML puro hospedado no Vercel)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCYDGROxguHYX-YA-J-HqRRGSF3uN-ZEAs",
   authDomain: "fenix-construtora-a34b5.firebaseapp.com",
   projectId: "fenix-construtora-a34b5",
-  storageBucket: "fenix-construtora-a34b5.appspot.com",
+  storageBucket: "fenix-construtora-a34b5.firebasestorage.app",
   messagingSenderId: "928009241790",
   appId: "1:928009241790:web:333b16b217a2ece01d8aef"
 };
@@ -14,5 +15,6 @@ const firebaseConfig = {
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta o Auth para usar em outros scripts
+// Exporta para usar em outros scripts
 export const auth = getAuth(app);
+export const db = getFirestore(app);
