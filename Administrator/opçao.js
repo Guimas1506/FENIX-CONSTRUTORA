d1dfcf86f6f669106ca0537b700935d07824103c
unknown <guilhermesilvamedeiro3@gmail.com>
2025-10-06 12:17:46 -0300

    atualização do botao do gerenciamneto

---
+++ Administrator/opçao.js
@@ -0,0 +1,6 @@
+const opcaoGerente = document.querySelector (".options-gerence")
+const seta = document.getElementById ("seta")
+
+seta.addEventListener("click", () => {
+    seta.style.rotate ("270deg")
+})
