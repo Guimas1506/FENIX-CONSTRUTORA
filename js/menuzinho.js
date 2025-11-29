40d035127d9722d04d239d4a3597145d89919acf
AquilesFreitas <aquilesfreitas350@gmail.com>
2025-11-29 18:44:57 -0300

    correção

--- js/menuzinho.js
+++ js/menuzinho.js
@@ -5,19 +5,16 @@ const sideMenu = document.getElementById("sideMenu");
 const overlay = document.getElementById("overlay");
 
 openMenu.addEventListener("click", () => {
-
   sideMenu.classList.add("open");
   overlay.classList.add("show");
 });
 
 closeMenu.addEventListener("click", () => {
-
   sideMenu.classList.remove("open");
   overlay.classList.remove("show");
 });
 
 overlay.addEventListener("click", () => {
-  sideMenu.style.display = "none"
   sideMenu.classList.remove("open");
   overlay.classList.remove("show");
 });
